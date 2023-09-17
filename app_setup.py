407eefe36c917c1219ae806b82fc48483caac146
Aleksei Brutov <mjbcaepio@gmail.com>
2023-09-17 19:30:10 +0300

    Сделал небольшое приложение, позволяющее отображать имя и текст поста юзера по его id.

--- app_setup.py
+++ app_setup.py
@@ -4,4 +4,4 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-conn = psycopg2.connect(dbname='travellist', user='travellist_user', password='password', host='172.20.0.3')
+conn = psycopg2.connect(dbname='travellist', user='travellist_user', password='password', host='172.20.0.2')
--- users.py
+++ users.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify
+from flask import request, jsonify, render_template
 
 from app_setup import app, conn
 
@@ -80,6 +80,48 @@ def get_user_info():
     user_info = cur.fetchall()
 
     return render_template('user_info_response.html', user_info=user_info)
+
+
+@app.post('/alter_table_users_add_foreign_key')
+def alter_table_users_add_foreign_key():
+    cur = conn.cursor()
+    cur.execute('ALTER TABLE users ADD FOREIGN KEY (post_id) REFERENCES posts(id);')
+    conn.commit()
+    response_data = {'status': 'table altered'}
+    return jsonify(response_data), 201
+
+
+@app.put('/update_user/<int:id>')
+def update_user(id):
+    cur = conn.cursor()
+    cur.execute('UPDATE users SET name = %s, age = %s WHERE id = %s;',
+                (request.json['name'], request.json['age'], id))
+
+    conn.commit()
+    response_data = {'status': 'user updated'}
+    return jsonify(response_data), 201
+
+
+class PostgresContextManager():
+    def __init__(self):
+        self.cursor = None
+
+    def __enter__(self):
+        cursor = conn.cursor()
+        self.cursor = cursor
+        return cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cursor.close()
+
+
+@app.get('/get_users_with_context_manager')
+def get_users_with_context_manager():
+    with PostgresContextManager() as cursor:
+        cursor.execute('SELECT * FROM users;')
+        users = cursor.fetchall()
+        response_data = {'users': users}
+        return jsonify(response_data), 200
 
 
 if __name__ == '__main__':
